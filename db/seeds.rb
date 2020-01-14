308c22d9a75f574cdaccbeefacac01d912c83cf7
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-01-14 19:13:37 -0300

    Seeds improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Restaurante.create([
-  {
-    name: Faker::Movies::LordOfTheRings.unique.location
-  },
-  {
-    name: Faker::Movies::LordOfTheRings.unique.location
-  },
-  {
-    name: Faker::Movies::LordOfTheRings.unique.location
-  },
-  {
-    name: Faker::Movies::LordOfTheRings.unique.location
-  }
-])
+10.times do
+  Restaurante.create([
+    {
+      name: Faker::Movies::LordOfTheRings.unique.location
+    }
+  ])
+end
 
 restaurants = Restaurante.all
 Dish.create([
